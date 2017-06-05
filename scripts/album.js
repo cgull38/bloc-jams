b0e7dbd77516946067a230178a25b47fe01a75d3
Corey Guthrie <cguthriedev@gmail.com>
2017-06-05 13:22:15 -0500

    Checkpoint 12 DOM Scripting Play/Pause Part 1: Complete

--- scripts/album.js
+++ scripts/album.js
@@ -30,10 +30,25 @@ var albumMarconi = {
     ]
 };
 
+var albumKendrick = {
+    title: 'DAMN',
+    artist: 'Kendrick Lamar',
+    label: 'Interscope',
+    year: '2017',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'BLOOD', duration: '1:58' },
+        { title: 'DNA', duration: '3:05' },
+        { title: 'YAH', duration: '2:40' },
+        { title: 'ELEMENT', duration: '3:28' },
+        { title: 'FEEL', duration: '3:34' }
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
         '<tr class="album-view-song-item">'
-    +   '   <td class="song-item-number">' + songNumber + '</td>'
+    +   '   <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
     +   '   <td class="song-item-title">' + songName + '</td>'
     +   '   <td class="song-item-duration">' + songLength + '</td>'
     +   '</tr>'
@@ -42,13 +57,15 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
+//Assign the corresponding values of the album object's properties to the HTML elements.
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 var setCurrentAlbum = function(album) {
-    //Assign the corresponding values of the album object's properties to the HTML elements.
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
     //Set the value of the first child node of the given element.
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
@@ -62,6 +79,37 @@ var setCurrentAlbum = function(album) {
     }
 };
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+//Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+
+    songListContainer.addEventListener('mouseover', function(event) {
+        // Only target individual song rows during event delegation
+        if (event.target.parentElement.className === 'album-view-song-item') {
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+        }
+    });
+
+    for (var i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseleave', function(event) {
+            //Selects first child element, which is the song-item-number element
+            this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+        });
+    }
+
+    var albumList = [albumPicasso, albumMarconi, albumKendrick];
+    var index = 1;
+
+    albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(albumList[index]);
+        index++;
+        if (index == albumList.length) {
+            index = 0;
+        }
+    });
 };
