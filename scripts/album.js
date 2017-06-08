6808cccdf9ef1d342488c4d18f2dec728776a2e9
Corey Guthrie <cguthriedev@gmail.com>
2017-06-08 09:00:55 -0500

    Assignment 13 DOM Scripting Play/Pause Part 2: Complete

--- scripts/album.js
+++ scripts/album.js
@@ -80,12 +80,19 @@ var setCurrentAlbum = function(album) {
 };
 
 var findParentByClassName = function(element, targetClass) {
-    if (element) {
-        var currentParent = element.parentElement;
+    var currentParent = element.parentElement;
+
+    if (currentParent.className === null) {
+        console.log("No parent found");
+
+    } else if (element) {
         while (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
         return currentParent;
+
+    } else {
+        console.log("No parent found with that class name")
     }
 };
 
